0da9e1cffc4f6fefd37b525f715d71175c7cca76
soepaza <soefreestyle@gmail.com>
2024-02-21 01:15:57 -0300

    Att projeto

--- Onepage.py
+++ Onepage.py
@@ -8,6 +8,7 @@
 import pandas as pd
 import time
 import pathlib
+import win32com.client as win32
 
 emails = pd.read_excel(r'Bases de Dados\Emails.xlsx')
 lojas = pd.read_csv(r'Bases de Dados\Lojas.csv', encoding='latin1', sep=';')
@@ -27,16 +28,16 @@ for loja in lojas['Loja']:
 dia_indicador = vendas['Data'].max()
 # print(dia_indicador())
 
-    # começar a criar o backup diario de quando fazer o resumo na onepage
+# começar a criar o backup diario de quando fazer o resumo na onepage
 caminho_backup = pathlib.Path(r'Backup Arquivos Lojas')
 # percorre a lista de arquivos ja existentes
 arquivos_existentes = caminho_backup.iterdir()
 
-    # criando lista que contem todos shoppings ja existentes
+# criando lista que contem todos shoppings ja existentes
 lista_shoppings = [shopping.name.strip() for shopping in arquivos_existentes]
 # print(lista_shoppings)
 
-    #criando a pasta do shopping com a lista de shoppings existes(ou nao existentes)
+# criando a pasta do shopping com a lista de shoppings existes(ou nao existentes)
 for loja in dicionario_lojas:
     loja_formatada = loja.strip()
     try:
@@ -44,29 +45,100 @@ for loja in dicionario_lojas:
             nova_pasta = caminho_backup / loja_formatada
             nova_pasta.mkdir()
 
-            #criar o arquivo (#Onepage) dentro da pasta do shopping.
-        nome_Onepage = "{}_{}_{}.xlsx".format(dia_indicador.month, dia_indicador.day, loja_formatada)
-        local_arquivo = caminho_backup / loja_formatada / nome_Onepage #"C:/Users/Home/../17_02_Loja.xlsx"
-            #mandar para o framework do python (dicionario)
+            # criar o arquivo (#Onepage) dentro da pasta do shopping.
+        nome_Onepage = "{}_{}_{}.xlsx".format(
+            dia_indicador.month, dia_indicador.day, loja_formatada)
+        local_arquivo = caminho_backup / loja_formatada / \
+            nome_Onepage  # "C:/Users/Home/../17_02_Loja.xlsx"
+        # mandar para o framework do python (dicionario)
         dicionario_lojas[loja].to_excel(local_arquivo)
     except FileExistsError:
         print('Continuando...')
 
-    #calcular indicador 1 (faturamento)
+    # calcular indicador 1 (faturamento)
+meta_faturamento_dia = 1000
+meta_faturamento_ano = 1650000
+meta_qtdeprodutos_dia = 4
+meta_qtdeprodutos_ano = 120
+meta_ticketmedio_dia = 500
+meta_ticketmedio_ano = 500
+
 loja = 'Norte Shopping'
 vendas_loja = dicionario_lojas[loja]
-vendas_loja_dia = vendas_loja.loc[vendas_loja['Data']==dia_indicador,:]
+vendas_loja_dia = vendas_loja.loc[vendas_loja['Data'] == dia_indicador, :]
+
 faturamento_ano = vendas_loja['Valor Final'].sum()
 faturamento_dia = vendas_loja_dia['Valor Final'].sum()
 
-    #calcular diversidade de produtos
-qtd_produtos_ano = len(vendas_loja['Produto'].unique())
+# calcular diversidade de produtos
+qtde_produtos_ano = len(vendas_loja['Produto'].unique())
+qtde_produtos_dia = len(vendas_loja_dia['Produto'].unique())
 
-qtd_produtos_ano_dia = len(vendas_loja_dia['Produto'].unique())
+# Calcular ticket médio
+valor_venda = vendas_loja.groupby('Código Venda')['Valor Final'].sum()
+valor_venda_dia = vendas_loja_dia.groupby('Código Venda')['Valor Final'].sum()
 
-    #calcular ticket medio
-valor_venda = vendas_loja.groupby['Código Venda'].sum()
-ticket_medio_ano = valor_venda['Valor Final'].mean()
+ticket_medio_ano = valor_venda.mean()
+ticket_medio_dia = valor_venda_dia.mean()
 
-valor_venda_dia = vendas_loja_dia.groupby['Código Venda'].sum()
-ticket_medio_dia = vendas_loja_dia['Valor Final'].mean()
+# Criar email para enviar os relatórios
+# criando instancia com outlook
+outlook = win32.Dispatch('Outlook.Application')
+email = outlook.CreateItem(0)
+
+nome = emails.loc[emails['Loja'] == loja, 'Gerente'].values[0]
+email.To = emails.loc[emails['Loja'] == loja, 'E-mail'].values[0]
+email.Subject = f'OnePage dia: {dia_indicador.day}/{dia_indicador.month} - Loja {loja}'
+
+if faturamento_dia >= meta_faturamento_dia and qtde_produtos_dia >= meta_qtdeprodutos_dia and ticket_medio_dia >= meta_ticketmedio_dia:
+    cor_fat_dia = 'green'
+    cor_qtd_dia = 'green'
+    cor_ticket_dia = 'green'
+else:
+    cor_fat_dia = 'red'
+    cor_qtd_dia = 'red'
+    cor_ticket_dia = 'red'
+
+email.HTMLBody = f'''
+<p>Bom dia, <strong>{nome}</strong> </p>
+
+<p>O resultado de ontem <strong>({dia_indicador.day}/{dia_indicador.month})</strong> da <strong>loja {loja}</strong> foi: </p>
+
+<table>
+  <tr>
+    <th>Indicador</th>
+    <th>Valor dia</th>
+    <th>Meta dia</th>
+    <th>Cenário dia</th>
+  </tr>
+  <tr>
+    <td>Faturamento</td>
+    <td style="text-align:center">{faturamento_dia}</td>
+    <td style="text-align:center">{meta_faturamento_dia}</td>
+    <td style="text-align:center"><font color={cor_fat_dia}>◙</font></td> 
+  </tr>
+  <tr>
+    <td>Diversidade de Produtos</td>
+    <td style="text-align:center">{qtde_produtos_dia}</td>
+    <td style="text-align:center">{meta_qtdeprodutos_dia}</td>
+    <td style="text-align:center"><font color={cor_qtd_dia}>◙</font></td> 
+  </tr>
+  <tr>
+    <td>Ticket Médio</td>
+    <td style="text-align:center">{ticket_medio_dia}</td>
+    <td style="text-align:center">{meta_ticketmedio_dia}</td>
+    <td style="text-align:center"><font color={cor_ticket_dia}>◙</font></td> 
+  </tr>
+ 
+</table>
+
+
+<p> Segue em anexo a planilha com todos os dados para mais detalhes. </p>
+<p> Qualquer dúvida estou a disposição.</p>
+<p> Att., Soe</p>
+'''
+attachment = pathlib.Path.cwd() / caminho_backup / loja / \
+    f'{dia_indicador.month}_{dia_indicador.day}_{loja}.xlsx'
+email.Attachments.Add(str(attachment))
+
+email.Send()
